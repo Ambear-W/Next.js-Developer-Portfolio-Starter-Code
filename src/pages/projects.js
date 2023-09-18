24805e4eb82978325671ddb8c6bf1cbdb7cf724d
Amber W <43912805+Ambear-W@users.noreply.github.com>
2023-09-18 17:44:21 -0400

    updated till 3 hour mark

---
+++ src/pages/projects.js
@@ -0,0 +1,88 @@
+import AnimatedText from '@/components/AnimatedText'
+import FeatureProject from '@/components/FeatureProject'
+import Layout from '@/components/Layout'
+import Head from 'next/head'
+import React from 'react'
+import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg"
+import project2 from "../../public/images/projects/crypto-screener-cover-image.jpg"
+import Project from '@/components/Project'
+
+const projects = () => {
+  return (
+    <>
+        <Head>
+            <title>CodeBucks | Projects Page</title>
+            <meta name="description" content="This is the projects page :)" />
+        </Head>
+        <main className='w-full mb-16 flex flex-col items-center justify-center'>
+            <Layout className='pt-16'>
+                <AnimatedText className='mb-16' text="Imagination Trumps Knowledge!" />
+
+                <div className='grid grid-cols-12 gap-24 gap-y-32'>
+                    <div className='col-span-12'>
+                        <FeatureProject 
+                        title="Crypto Screener Application"
+                        summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                        local currency."
+                        link="/"
+                        github="/"
+                        type="Featured Project"
+                        img={project1}                        
+                        />
+                    </div>
+                    <div className='col-span-6'>
+                        <Project
+                        title="React Portfolio Website"
+                        link="/"
+                        github="/"
+                        type="Project"
+                        img={project2}
+                        />
+                    </div>
+                    <div className='col-span-6'>
+                        <Project
+                        title="React Portfolio Website"
+                        link="/"
+                        github="/"
+                        type="Project"
+                        img={project2}
+                        />
+                    </div>
+
+                    <div className='col-span-12'>
+                        <FeatureProject 
+                        title="Crypto Screener Application"
+                        summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                        local currency."
+                        link="/"
+                        github="/"
+                        type="Featured Project"
+                        img={project1}                        
+                        />
+                    </div>
+                    <div className='col-span-6'>
+                        <Project
+                        title="React Portfolio Website"
+                        link="/"
+                        github="/"
+                        type="Project"
+                        img={project2}
+                        />
+                    </div>
+                    <div className='col-span-6'>
+                        <Project
+                        title="React Portfolio Website"
+                        link="/"
+                        github="/"
+                        type="Project"
+                        img={project2}
+                        />
+                    </div>
+                </div>
+            </Layout>
+        </main>
+    </>
+  )
+}
+
+export default projects
